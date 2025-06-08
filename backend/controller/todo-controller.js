d43e1a1a81a37d512afe1b1485f302826df2ba1a
Ricardo Castillo <ricardoacz20@gmail.com>
2025-06-08 19:11:51 -0400

    Create basic todo routes and controller placeholders

--- backend/controller/todo-controller.js
+++ backend/controller/todo-controller.js
@@ -3,3 +3,32 @@
 export const getTodos = async (req, res) => {
     res.send('Todos would be here')
 }
+
+export const createTodo = async (req, res) => {
+    res.send('Todos would be here')
+}
+
+export const updateTodo = async (req, res) => {
+    res.send('Todos would be here')
+}
+
+export const markTodoDone = async (req, res) => {
+    res.send('Todos would be here')
+}
+
+export const unmarkTodoDone = async (req, res) => {
+    res.send('Todos would be here')
+}
+
+export const markTodoImportant = async (req, res) => {
+    res.send('Todos would be here')
+}
+
+export const unmarkTodoImportant = async (req, res) => {
+    res.send('Todos would be here')
+}
+
+export const deleteTodo = async (req, res) => {
+    res.send('Todos would be here')
+}
+
--- backend/routes/todo-routes.js
+++ backend/routes/todo-routes.js
@@ -1,8 +1,18 @@
 import express from 'express'
-import { getTodos } from '../controller/todo-controller.js'
+import { createTodo, deleteTodo, getTodos, markTodoDone, markTodoImportant, unmarkTodoDone, unmarkTodoImportant, updateTodo } from '../controller/todo-controller.js'
 
 const router = express.Router()
 
 router.get('/', getTodos)
+
+router.post('/create', createTodo)
+
+router.put('/id', updateTodo)
+router.put('/id', markTodoDone)
+router.put('/id', unmarkTodoDone)
+router.put('/id', markTodoImportant)
+router.put('/id', unmarkTodoImportant)
+
+router.delete('/id', deleteTodo)
 
 export default router
