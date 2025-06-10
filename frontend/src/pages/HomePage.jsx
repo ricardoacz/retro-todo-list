523e2ee286f11b498569fe330d8ff7d44722ac1b
Ricardo Castillo <ricardoacz20@gmail.com>
2025-06-09 22:01:54 -0400

    Fix useEffect session refresh issue

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 import {useAuth} from '../global_state/authStore'
+import { account } from '../appwriteConfig'
 
 import Todo from '../components/Todo'
 
@@ -8,24 +9,38 @@ function HomePage() {
     
     const {logoutUser, checkUserStatus, loading, user} = useAuth()
 
-    
+    const [todoValue, setTodoValue] = useState("")
     
     const navigate = useNavigate()
 
-    // Trigger check user logged in
-    useEffect(() => {
-        const check = async () => {
-        await checkUserStatus()
-        }
-        check()
-    }, [])
+    // // Trigger check user logged in
+    // useEffect(() => {
+    //     const check = async () => {
+    //     await checkUserStatus()
+    //     }
+    //     check()
+    // }, [])
 
-    // Comfirm user is logged in and redirect
+    // // Comfirm user is logged in and redirect
+    // useEffect(() => {
+    //     if (loading === false && user === null) {
+    //     navigate('/login')
+    //     }
+    // }, [loading, user])
+
     useEffect(() => {
-        if (!loading && !user) {
-        navigate('/login')
+        const checkUserSession = async () => {
+            let session = null
+            try {
+                session = await account.get()
+                console.log(session)
+                
+            } catch (error) {
+                navigate('/login')
+            }  
         }
-    }, [loading, user])
+        checkUserSession()
+    }, [])
 
     const testData = [
         {
@@ -48,13 +63,52 @@ function HomePage() {
         }
     ]
 
+    const [todos, setTodos] = useState([
+        {
+            todo: 'buy oreos',
+            user: 'John',
+            important: false,
+            completed: false
+        },
+        {
+            todo: 'sell old phone',
+            user: 'John',
+            important: true,
+            completed: false
+        },
+        {
+            todo: 'go out with friends',
+            user: 'John',
+            important: false,
+            completed: true
+        }
+    ])
+
+    const handleAddTodo = async (e) => {
+        e.preventDefault()
+        const todoTemplate = {
+            todo: '',
+            user: 'John',
+            important: false,
+            completed: false
+        }
+        const copyTodos = todos.slice()
+        setTodos([...copyTodos, {...todoTemplate, todo: todoValue}])
+    }
+
     return (
         <div>
             <button onClick={logoutUser}>Logout</button>
             <button>Light Mode</button>
             <h1>Welcome Nickname</h1>
             <div>
-                {testData.map((todo, index) => (
+                <form onSubmit={handleAddTodo}>
+                    <input onChange={(e) => setTodoValue(e.target.value)} placeholder='todo' />
+                    <button>Submit</button>
+                </form>
+            </div>
+            <div>
+                {todos.map((todo, index) => (
                     <Todo key={index} todo={todo} />
                 ))}
             </div>
